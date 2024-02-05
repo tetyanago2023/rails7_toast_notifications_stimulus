042149737ad9e14ecb6e64400a9cd51272cbb63b
go2024 <tetyanago2024@gmail.com>
2024-02-04 18:00:16 -0800

    Post stuff was created

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import NotificationsController from "./notifications_controller"
+application.register("notifications", NotificationsController)
